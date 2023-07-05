8547457803b89cfa781cef83e32808c99cbc5c4c
Erison88 <songerison@gmail.com>
2023-07-05 23:40:19 +0800

    add seven_seg module

--- Verilog_practice/Lab1.v
+++ Verilog_practice/Lab1.v
@@ -65,6 +65,27 @@ module simple(a, b, Cin, SW, sum, LEDR, SEVEN_SEGMENT_DIGIT);
 	 
 endmodule
 
+module seven_seg(in, seg);
+input [3:0] in;
+output [6:0] seg;
+always@(in)
+begin
+    case(in)
+        4'b0000: seg = 7'b1000000;
+        4'b0001: seg = 7'b1111001;
+        4'b0010: seg = 7'b0100100;
+        4'b0011: seg = 7'b0110000;
+        4'b0100: seg = 7'b0011001;
+        4'b0101: seg = 7'b0010010;
+        4'b0110: seg = 7'b0000010;
+        4'b0111: seg = 7'b1111000;
+        4'b1000: seg = 7'b0000000;
+        4'b1001: seg = 7'b0010000;
+        default: seg = 7'b1111111; // blank
+    endcase
+end
+endmodule
+
 
 // 課本的
 module example verilog (SW, LEDR, LEDG);
