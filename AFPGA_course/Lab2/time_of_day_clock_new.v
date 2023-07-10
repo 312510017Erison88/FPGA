195f72fbd70b7698fb49dade59ed3caf203dcfb1
Erison88 <songerison@gmail.com>
2023-07-10 11:47:06 +0800

    HEX0~5 declare output, not output reg

--- AFPGA_course/Lab2/time_of_day_clock_new.v
+++ AFPGA_course/Lab2/time_of_day_clock_new.v
@@ -1,22 +1,30 @@
 module time_of_day_clock(
   input [9:0] SW,
-  input clk,
-  input [1:0] KEY,        // KEY is reset
-  output reg [6:0] HEX0,
-  output reg [6:0] HEX1,
-  output reg [6:0] HEX2,
-  output reg [6:0] HEX3,
-  output reg [6:0] HEX4,
-  output reg [6:0] HEX5
+  input CLOCK_50,
+  input [1:0] KEY,      // KEY is reset
+  output [6:0] HEX0,
+  output [6:0] HEX1,
+  output [6:0] HEX2,
+  output [6:0] HEX3,
+  output [6:0] HEX4,
+  output [6:0] HEX5
 );
+	
+	wire CLK_1HZ, CLK_1000HZ;
+    clk_divider clk_divider_uut0(CLOCK_50, CLK_1HZ);
+    clk_divider clk_divider_uut1(CLOCK_50, CLK_1000HZ);
+    defparam clk_divider_uut1.freq = 1000;
 
+    wire SLOW_CLK;
+    assign SLOW_CLK = (KEY[1]) ? CLK_1HZ : CLK_1000HZ;
+	 
     // current_time
     reg [3:0] hour_1, hour_0, minute_1, minute_0, second_1, second_0;
     // user setting time
     reg [3:0] set_hour_1, set_hour_0, set_minute_1, set_minute_0, set_second_1, set_second_0;
     
     // determine the set_time condition
-    always@ (posedge clk) begin
+    always@ (posedge CLOCK_50) begin
         if(SW[9]) begin
             if(SW[3:0] <= 4'd9)
                 set_hour_0 <= SW[3:0];
@@ -44,7 +52,7 @@ module time_of_day_clock(
     // priority: reset > set > count
     wire reset;
     assign reset = KEY[0];
-    always@ ( posedge reset, posedge SW[8], posedge clk) begin
+    always@( posedge reset, posedge SW[8], posedge SLOW_CLK) begin
         if(reset)begin
             hour_1 <= 4'd0;
             hour_0 <= 4'd0;
@@ -93,12 +101,12 @@ module time_of_day_clock(
     end
 
     // Display hours on HEX5 and HEX4
-    BCD_to_seven_segment_1 display_hour_1(hour_1, HEX5);
-    BCD_to_seven_segment_1 display_hour_0(hour_0, HEX4);
+    BCD_to_seven_segment display_hour_1(hour_1, HEX5);
+    BCD_to_seven_segment display_hour_0(hour_0, HEX4);
     // Display minutes on HEX3 and HEX2
-    BCD_to_seven_segment_1 display_minute_1(minute_1, HEX3);
-    BCD_to_seven_segment_1 display_minute_0(minute_0, HEX2);
+    BCD_to_seven_segment display_minute_1(minute_1, HEX3);
+    BCD_to_seven_segment display_minute_0(minute_0, HEX2);
     // Display seconds on HEX1 and HEX0
-    BCD_to_seven_segment_1 display_second_1(second_1, HEX1);
-    BCD_to_seven_segment_1 display_second_0(second_0, HEX0);
+    BCD_to_seven_segment display_second_1(second_1, HEX1);
+    BCD_to_seven_segment display_second_0(second_0, HEX0);
 endmodule
