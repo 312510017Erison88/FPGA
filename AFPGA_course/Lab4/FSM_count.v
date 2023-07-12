3c8179e57729ccb41fd452a6b16b7e7efa9f8c37
Erison88 <songerison@gmail.com>
2023-07-12 21:37:00 +0800

    add my vision instead chatgpt

--- AFPGA_course/Lab4/FSM_count.v
+++ AFPGA_course/Lab4/FSM_count.v
@@ -1,50 +1,97 @@
-// chatgpt vision
 module counter(
   input [2:0] SW,
   input [0:0] KEY,      // KEY is CLOCK
   output [6:0] HEX[0],
 );
-    
+    // reset
     wire reset;
     assign reset = !SW[0];
+
+    // Clock
+    wire CLK;
+    assign CLK = !KEY[0];
     
-    input [1:0] w,
-    input manual_clock,
-    output reg [3:0] count
-    reg [2:0] state;
+    // input condition 
+    wire [1:0] in,
+    assign in = {SW[2], SW[1]};
+
+    // state name
+    parameter [1:0] Add_one = 2'b00,
+                    Add_two = 2'b01,
+                    Sub_one = 2'b10,
+                    No_change = 2'b11;
+    
+    reg [3:0] count
+    reg [1:0] cur_state;
     reg [1:0] next_state;
 
-    always @(posedge clk) begin
-    if (reset)
-        state <= 0;
+    // current state machine
+    always @(posedge CLK) begin
+    if(reset)
+        cur_state <= 0;
     else
-        state <= next_state;
+        cur_state <= next_state;
     end
 
-    always @(state, w, manual_clock) begin
-    case (state)
-        3'b000: begin
-        next_state = (w == 2'b00) ? 3'b000 : 3'b001;
+    // sub-state machine
+    always @(cur_state, w, CLK) begin   // CLK can take off ?
+    case (cur_state)
+        Add_one: begin
+        if(w == Add_one)
+            next_state <= Add_one;
+        else if(w == Add_two)
+            next_state <= Add_two;
+        else if(w == Sub_one)
+            next_state <= Sub_one;
+        else
+            next_state <= No_change;
         end
-        3'b001: begin
-        next_state = (w == 2'b00) ? 3'b001 : 3'b010;
+        Add_two: begin
+        if(w == Add_one)
+            next_state <= Add_one;
+        else if(w == Add_two)
+            next_state <= Add_two;
+        else if(w == Sub_one)
+            next_state <= Sub_one;
+        else
+            next_state <= No_change;
         end
-        3'b010: begin
-        next_state = (w == 2'b00) ? 3'b010 : 3'b011;
+        Sub_one: begin
+        if(w == Add_one)
+            next_state <= Add_one;
+        else if(w == Add_two)
+            next_state <= Add_two;
+        else if(w == Sub_one)
+            next_state <= Sub_one;
+        else
+            next_state <= No_change;
         end
-        3'b011: begin
-        next_state = (w == 2'b00) ? 3'b011 : 3'b000;
+        No_change: begin
+        if(w == Add_one)
+            next_state <= Add_one;
+        else if(w == Add_two)
+            next_state <= Add_two;
+        else if(w == Sub_one)
+            next_state <= Sub_one;
+        else
+            next_state <= No_change;
         end
     endcase
     end
 
-    always @(posedge manual_clock) begin
-    if (state == 3'b011)
+    // output logic
+    always @(posedge CLK) begin
+    if (cur_state == Add_one)
         count <= (count == 4'b1001) ? 4'b0000 : count + 1;
-    else if (state == 3'b010)
+    else if (cur_state == Add_two)
         count <= (count == 4'b1000) ? 4'b0000 : count + 2;
-    else if (state == 3'b001)
+    else if (cur_state == Sub_one)
         count <= (count == 4'b0000) ? 4'b1001 : count - 1;
+    else
+        count <= count;
     end
+
+    // display on HEX0
+    BCD_to_seven_segment(count, HEX0);
 
 endmodule
