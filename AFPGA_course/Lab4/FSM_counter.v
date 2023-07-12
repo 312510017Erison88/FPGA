7ec761496f2564ba84c24b6db9f22d2af523c60a
Erison88 <songerison@gmail.com>
2023-07-12 22:55:08 +0800

    correct small error

--- AFPGA_course/Lab4/FSM_counter.v
+++ AFPGA_course/Lab4/FSM_counter.v
@@ -1,7 +1,7 @@
 module FSM_counter(
   input [2:0] SW,
   input [0:0] KEY,      // KEY is CLOCK
-  output [6:0] HEX[0],
+  output [6:0] HEX0
 );
     // reset
     wire reset;
@@ -12,83 +12,90 @@ module FSM_counter(
     assign CLK = !KEY[0];
     
     // input condition 
-    wire [1:0] in,
+    wire [1:0] in;
     assign in = {SW[2], SW[1]};
 
     // state name
-    parameter [1:0] Add_one = 2'b00,
-                    Add_two = 2'b01,
-                    Sub_one = 2'b10,
-                    No_change = 2'b11;
+    parameter [1:0] No_change = 2'b00,
+                    Add_one = 2'b01,
+                    Add_two = 2'b10,
+                    Sub_one = 2'b11;
     
-    reg [3:0] count
+    reg [3:0] count;
     reg [1:0] cur_state;
     reg [1:0] next_state;
+    //reg [1:0] pre_in;
 
     // current state machine
-    always @(posedge CLK) begin
-    if(reset)
-        cur_state <= 0;
-    else
-        cur_state <= next_state;
+    always @(posedge CLK or posedge reset) begin
+        if(reset) begin
+            //cur_state <= No_change;
+            count <= 4'd0;
+        end
+        else begin
+            cur_state <= next_state;
+            // output logic
+            case (cur_state)
+            Add_one:
+                count <= (count == 4'b1001) ? 4'b0000 : count + 1;
+            Add_two: begin
+                if (count == 4'd8)
+                    count <= 4'd0;
+                else if (count == 4'd9)
+                    count <= 4'd1;
+                end
+            Sub_one:
+                count <= (count == 4'b0000) ? 4'b1001 : count - 1;
+            default:
+                count <= count;
+        endcase
+        end
     end
 
-    // sub-state machine
-    always @(cur_state, w, CLK) begin   // CLK can take off ?
-    case (cur_state)
-        Add_one: begin
-        if(w == Add_one)
-            next_state <= Add_one;
-        else if(w == Add_two)
-            next_state <= Add_two;
-        else if(w == Sub_one)
-            next_state <= Sub_one;
-        else
-            next_state <= No_change;
-        end
-        Add_two: begin
-        if(w == Add_one)
-            next_state <= Add_one;
-        else if(w == Add_two)
-            next_state <= Add_two;
-        else if(w == Sub_one)
-            next_state <= Sub_one;
-        else
-            next_state <= No_change;
-        end
-        Sub_one: begin
-        if(w == Add_one)
-            next_state <= Add_one;
-        else if(w == Add_two)
-            next_state <= Add_two;
-        else if(w == Sub_one)
-            next_state <= Sub_one;
-        else
-            next_state <= No_change;
-        end
-        No_change: begin
-        if(w == Add_one)
-            next_state <= Add_one;
-        else if(w == Add_two)
-            next_state <= Add_two;
-        else if(w == Sub_one)
-            next_state <= Sub_one;
-        else
-            next_state <= No_change;
-        end
-    endcase
-    end
-
-    // output logic
-    always @(posedge CLK) begin
-    if (cur_state == Add_one)
-        count <= (count == 4'b1001) ? 4'b0000 : count + 1;
-    else if (cur_state == Add_two)
-        count <= (count == 4'b1000) ? 4'b0000 : count + 2;
-    else if (cur_state == Sub_one)
-        count <= (count == 4'b0000) ? 4'b1001 : count - 1;
-    else
-        count <= count;
+    // sub-state machine (state transition)
+    always @(cur_state or in) begin   
+        case (cur_state)
+            Add_one: begin
+                if(in == Add_one)
+                    next_state = Add_one;
+                else if(in == Add_two)
+                    next_state = Add_two;
+                else if(in == Sub_one)
+                    next_state = Sub_one;
+                else
+                    next_state = No_change;
+            end
+            Add_two: begin
+                if(in == Add_one)
+                    next_state = Add_one;
+                else if(in == Add_two)
+                    next_state = Add_two;
+                else if(in == Sub_one)
+                    next_state = Sub_one;
+                else
+                    next_state = No_change;
+            end
+            Sub_one: begin
+                if(in == Add_one)
+                    next_state = Add_one;
+                else if(in == Add_two)
+                    next_state = Add_two;
+                else if(in == Sub_one)
+                    next_state = Sub_one;
+                else
+                    next_state = No_change;
+            end
+            No_change: begin
+                if(in == Add_one)
+                    next_state = Add_one;
+                else if(in == Add_two)
+                    next_state = Add_two;
+                else if(in == Sub_one)
+                    next_state = Sub_one;
+                else
+                    next_state = No_change;
+            end
+        endcase
     end
 
     // display on HEX0
